b7abdad4d8349a904a0a2d3d5f38eb9f73f6ce41
Dharmik7601 <62546865+Dharmik7601@users.noreply.github.com>
2023-02-01 11:56:22 +0530

    Added form database and did some changes in form.jsx.

--- frontend/src/pages/Formdata.jsx
+++ frontend/src/pages/Formdata.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 function Formdata() {
   const [name, setName] = useState(""); //separate out first and last name
@@ -14,6 +15,18 @@ function Formdata() {
     if (name === "loanType") setLoanType(value);
     if (name === "gender") setGender(value);
     if (name === "agreement") setAgreement(value);
+  }
+
+
+  // Created this submit form function which will post the data inputed in the form to the URL displayed below. Below url has a create form function which will collect the data from the form any create a new data in the form database.
+  function submitForm() {
+    if (name && loanType && gender && agreement) {
+      const form = { name, loanType, gender, agreement }; // here form object is created
+      axios.post("http://localhost:5000/form", form).then(response => console.log(response)) // Here the data is send to the url
+      //The only issue is that the response we are getting from the back function is not getting displayed. The page refresh after clicking the submit form button and i think that is the problem
+    } else {
+      alert("Invalid Input")
+    } 
   }
 
   return (
@@ -82,7 +95,8 @@ function Formdata() {
         </label>
         {/* submit button */}
         <div class="form-example">
-          <input type="submit" value="Submit" />
+          <input type="submit" onClick={submitForm} value="Submit" />
+          {/* Added the submit form function to the submit button */}
         </div>
       </form>
     </div>
