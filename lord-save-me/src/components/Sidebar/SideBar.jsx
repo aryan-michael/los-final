6eeb41cfae08d7bab9c08af2de29b990aeb23d7d
aryan-michael <aryanmichael24@gmail.com>
2023-02-22 17:45:22 +0530

    favicon and title edited

--- lord-save-me/src/components/Sidebar/SideBar.jsx
+++ lord-save-me/src/components/Sidebar/SideBar.jsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import SidebarMenu from "./SidebarMenu";
 import './SideBar.css';
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 const routes = [
   {
     path: "/",
@@ -17,7 +18,7 @@ const routes = [
   },
   {
     path: "/users",
-    name: "Users",
+    name: "My Account",
     icon: <FaUser />,
   },
   {
@@ -126,6 +127,50 @@ const SideBar = ({ children }) => {
 
   return (
     <>
+      <Navbar sticky="top" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            LOS
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Products" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/loan/business">
+                  Business Loan
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/loan/home">
+                  Home Loan
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/loan/education">
+                  Education Loan
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/loan/personal">
+                  Personal Loan
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Upcoming Products
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/sidebar">SideBar</Nav.Link>
+              <NavDropdown title="Login" id="collasible-nav-dropdown">
+                <NavDropdown.Item eventKey={2} href="/login/client">
+                  Client
+                </NavDropdown.Item>
+                <NavDropdown.Item href="">
+                  Admin
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <br />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div className="main-container">
         <motion.div
           animate={{
@@ -149,7 +194,7 @@ const SideBar = ({ children }) => {
                   exit="hidden"
                   className="logo"
                 >
-                  LOS
+                  MENU
                 </motion.h1>
               )}
             </AnimatePresence>
