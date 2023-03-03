dbb7eb234a4d78d40a80b8651eed553884861947
Dharmik7601 <62546865+Dharmik7601@users.noreply.github.com>
2023-03-03 16:08:28 +0530

    One more merger

---
+++ lord-save-me/src/pages/UserPanel/BankDetailsFinal.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import SideBar from "../../components/Sidebar/SideBar";
+import PostLoginNavBar from "../../components/NavBar/PostLoginNavBar";
+import { Form, Row } from 'react-bootstrap';
+
+
+const BankDetails = () => {
+
+    // const [error, setError] = useState({
+    //     account_number: '',
+    //     card_number: '',
+    //     cvv: '',
+    //     cardholder: '',
+    //     expiry_date: '',
+    // });
+
+    // const [bankDetails, setBankDetails] = useState({
+    //     account_number: '',
+    //     card_number: '',
+    //     cvv: '',
+    //     cardholder: '',
+    //     expiry_date: '',
+    // });
+
+
+    return (
+        <>
+            <PostLoginNavBar />
+            <div style={{ display: 'flex' }}>
+                <SideBar />
+                <Form className="container mt-3 mb-3" autoComplete='off'>
+                    <Row className="mb-3">
+                        {/* <Alert>{error}</Alert> */}
+                        <div className="title"> Bank Details </div>
+                    </Row>
+
+                    <Row className="mb-3">
+                        {/* BANK ACCOUNT NUMBER */}
+                        <Form.Group controlId="formBasicEmail" className="col col-sm-6">
+                            <Form.Label>Bank Account Number</Form.Label>
+                            <Form.Control type="number" name="account_number" className="form-control" readOnly />
+                        </Form.Group>
+
+                        {/* OTP */}
+                        {/* <Form.Group controlId="formGridpin" className="col col-sm-3">
+                            <Form.Label>Enter OTP</Form.Label>
+                            <Form.Control className="form-control" type="text" name="bank_otp" readOnly />
+                        </Form.Group>
+                        <Form.Group controlId="formGridCheckbox" className="col col-sm-2">
+                            <Button type="submit" className="me-4 mt-4 btn-xs">Check OTP</Button>
+                        </Form.Group> */}
+                    </Row>
+
+                    <Row className="mb-3">
+                        {/* CREDIT/DEBIT CARD INFO */}
+                        <Form.Group controlId="formBasicEmail" className="col col-sm-4">
+                            <Form.Label>Credit / Debit Card Number</Form.Label>
+                            <Form.Control type="number" name="card_number" className="form-control" readOnly />
+                        </Form.Group>
+                        {/* CVV */}
+                        <Form.Group controlId="formBasicEmail" className="col col-sm-1">
+                            <Form.Label>CVV</Form.Label>
+                            <Form.Control type="password" name="cvv" className="form-control" readOnly />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        {/* CARD HOLDER"S NAME*/}
+                        <Form.Group controlId="formGridpin" className="col col-sm-3">
+                            <Form.Label>Card Holder's Name</Form.Label>
+                            <Form.Control className="form-control" type="text" name="cardholder" readOnly />
+                        </Form.Group>
+                        {/* CARD EXPIRY DATE */}
+                        <Form.Group controlId="formGridpin" className="col col-sm-2">
+                            <Form.Label>Expiry Date</Form.Label>
+                            <Form.Control className="form-control" type="date" name="expiry_date" readOnly />
+                        </Form.Group>
+                    </Row>
+
+                </Form>
+            </div>
+        </>
+    );
+}
+
+export default BankDetails
+
+// 1. bank account number >> check button
+// 2. credit/debit card number >> check button
+// 3. cvv >> check button
+// 4. expiry date >> check button
--- lord-save-me/src/pages/UserPanel/KYC.jsx
+++ lord-save-me/src/pages/UserPanel/KYC.jsx
@@ -1,10 +1,11 @@
+import PostLoginNavBar from "../../components/NavBar/PostLoginNavBar";
 import SideBar from "../../components/Sidebar/SideBar";
-import NavBar from "../../components/NavBar/NavBar";
+
 
 const KYC = () => {
   return (
     <>
-      <NavBar />
+      <PostLoginNavBar />
       <div style={{ display: 'flex' }}>
         <SideBar />
         <div className="title"> KYC </div>
