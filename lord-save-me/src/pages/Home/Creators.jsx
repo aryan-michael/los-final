0fd89cb05f30b5be43f23ec79d75486c84a99969
aryan-michael <aryanmichael24@gmail.com>
2023-03-09 11:59:54 +0530

    device change

--- lord-save-me/src/pages/Home/Creators.jsx
+++ lord-save-me/src/pages/Home/Creators.jsx
@@ -1,42 +1,37 @@
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
-import NavBar from "../../components/NavBar/NavBar";
-import "./Creators.css";
+//import './Creators.css';
 
 export default function Creators() {
-    return (
+  return (
     <>
-    <NavBar />
-        <MDBCarousel showIndicators showControls fade className="list-item" >
-            <MDBCarouselItem
-                className='w-100 d-block'
-                itemId={1}
-                src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg'
-                alt='...'
-            >
-                <h5>Meet our creators</h5>
-                <p>We are final year engineering students at Indus University and we have built this application using the MERN stack</p>
-            </MDBCarouselItem>
+      <MDBCarousel showControls showIndicators>
+        <MDBCarouselItem
+          className='w-100 d-block'
+          itemId={1}
+          src='https://mdbootstrap.com/img/new/slides/041.jpg'
+          alt='...'>
+          <h4>1 slide label</h4>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </MDBCarouselItem>
 
-            <MDBCarouselItem
-                className='w-100 d-block'
-                itemId={1}
-                src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg'
-                alt='...'
-            >
-                <h5>Aryan Michael</h5>
-                <p>Frontend guy</p>
-            </MDBCarouselItem>
+        <MDBCarouselItem
+          className='w-100 d-block'
+          itemId={2}
+          src='https://mdbootstrap.com/img/new/slides/042.jpg'
+          alt='...'>
+          <h4>2 slide label</h4>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </MDBCarouselItem>
 
-            <MDBCarouselItem
-                className='w-100 d-block'
-                itemId={1}
-                src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg'
-                alt='...'
-            >
-                <h5>Dharmik Patel</h5>
-                <p>Backend guy</p>
-            </MDBCarouselItem>
-        </MDBCarousel>
+        <MDBCarouselItem
+          className='w-100 d-block'
+          itemId={3}
+          src='https://mdbootstrap.com/img/new/slides/043.jpg'
+          alt='...'>
+          <h4>3 slide label</h4>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </MDBCarouselItem>
+      </MDBCarousel>
     </>
-    );
+  );
 }
